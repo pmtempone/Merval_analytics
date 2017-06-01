59d05c16e2c4d383bf0c2ecadb26430f0731dfc5
pmtempone <pmtempone@gmail.com>
2017-05-31 23:28:16 -0300

    carga de serie

---
+++ 01 Generacion de datos.R
@@ -0,0 +1,27 @@
+# Time Series Plotting
+library(ggplot2)
+library(xts)
+library(dygraphs)
+library(quantmod)
+
+#listado de acciones para seguir: ferrum,havanna,pampa energia,acindar, byma,andes energy
+
+getQuote("GGAL;BMA", what=yahooQF(c("Bid","Ask","Last Trade (Price Only)")))
+
+
+
+plot(GOOG)
+
+
+
+getSymbols('BCBA:HAVA',src='google')
+getSymbols('BCBA:FERR',src='google')
+getSymbols('BCBA:PAMP',src='google')
+getSymbols('BCBA:BYMA',src='google')
+
+
+plot(`BCBA:HAVA`[,4],main="HAVANNA")
+
+plot(`BCBA:FERR`[,4],main="FERRUM")
+plot(`BCBA:PAMP`[,4],main="PAMPA")
+plot(`BCBA:BYMA`[,4],main="BYMA")
